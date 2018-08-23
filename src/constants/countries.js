74892f9428d0be8d68c775fea0cde21da6b3297b
Unknown <zanardo.florian@gmail.com>
2018-08-23 11:16:55 +0200

    feat(asian map): display country name and date when hover

---
+++ src/constants/countries.js
@@ -0,0 +1,78 @@
+export default {
+  bangladesh: {
+    name: 'Bangladesh',
+    approximateDate: 'Pas prévu'
+  },
+  bhutan: {
+    name: 'Bhoutan',
+    approximateDate: 'Pas prévu'
+  },
+  cambodia: {
+    name: 'Cambodge',
+    approximateDate: 'Mars 2018'
+  },
+  china: {
+    name: 'Chine',
+    approximateDate: 'Pas prévu'
+  },
+  indonesia: {
+    name: 'Indonésie',
+    approximateDate: 'Octobre 2017'
+  },
+  india: {
+    name: 'Inde',
+    approximateDate: 'Pas prévu'
+  },
+  japan: {
+    name: 'Japon',
+    approximateDate: 'Pas prévu'
+  },
+  laos: {
+    name: 'Laos',
+    approximateDate: 'Janvier 2018'
+  },
+  northKorea: {
+    name: 'North Korea',
+    approximateDate: 'Pas prévu'
+  },
+  southKorea: {
+    name: 'South Korea',
+    approximateDate: 'Pas prévu'
+  },
+  malaysia: {
+    name: 'Malaysia',
+    approximateDate: 'Juin-Juillet-Août 2018'
+  },
+  myanmar: {
+    name: 'Birmanie',
+    approximateDate: 'Décembre 2017'
+  },
+  nepal: {
+    name: 'Nepal',
+    approximateDate: 'Pas prévu'
+  },
+  papuaNewGuinea: {
+    name: 'Papouasie Nouvelle Guinée',
+    approximateDate: 'Pas prévu'
+  },
+  philippines: {
+    name: 'Philippines',
+    approximateDate: 'Avril 2018'
+  },
+  sriLanka: {
+    name: 'Sri Lanka',
+    approximateDate: 'Pas prévu'
+  },
+  taiwan: {
+    name: 'Taïwan',
+    approximateDate: 'Pas prévu'
+  },
+  thailand: {
+    name: 'Thaïlande',
+    approximateDate: 'Novembre 2017'
+  },
+  vietnam: {
+    name: 'Vietnam',
+    approximateDate: 'Février 2018'
+  }
+}
