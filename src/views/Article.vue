090044cf687166fabc3823bd0ded6fb2d0524af3
PestoP <zanardo.florian@gmail.com>
2018-11-07 15:53:41 +0100

    feat(articles): initialize articles

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,3 +1,16 @@
 <template>
-  <span>lol</span>
+  <component :is="articleComponentName" />
 </template>
+
+<script>
+import Sukhotai from './articles/thailand/Sukhotai.vue'
+
+export default {
+  components: {Sukhotai},
+  data () {
+    return {
+      articleComponentName: this.$route.params.articleName.charAt(0).toUpperCase() + this.$route.params.articleName.slice(1)
+    }
+  }
+}
+</script>
