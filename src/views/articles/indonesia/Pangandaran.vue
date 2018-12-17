e94d16b70807880763d11b634e296d33185d324b
PestoP <zanardo.florian@gmail.com>
2018-12-17 12:17:38 +0100

    feat(pangandaran article): here is the content

--- src/views/articles/indonesia/Pangandaran.vue
+++ src/views/articles/indonesia/Pangandaran.vue
@@ -1,3 +1,44 @@
 <template>
-  <span>lol</span>
+<v-container>
+
+  <article>
+    <h2>Le calme de Pangandaran</h2>
+    <h3>
+      Comment arriver à pangandaran depuis Jakarta ?
+    </h3>
+    <p>
+      Il faut savoir que Jakarta-Jogja, c’est environ 8h de transport. Si vous passez par pangandaran vous doublez ce temps. Même si j’ai adoré cette destination, je la déconseille pour des gens qui n’ont que 2 semaines pour faire l'Indonésie ! En effet, ils devraient aller directement à Yogyakarta, faire Borobudur et Prambanan puis partir direction le mont Bromo sans perdre de temps ;)
+    </p>
+    <p>
+      Mes amis et moi avons tout simplement réservé le trajet via Rome2Rio. Il s’est passé en 2 étapes : Jakarta-Bandung en train puis Bandung-Pangandaran en ‘Bus’. Je met des guillemets parce que finalement c’est une voiture 7 places qui est venue nous chercher ! 7h (au moins) de transport avec la conduite indonésienne. Devoir doubler des scooters pendant tout le trajet, 40 km/h de moyenne. Le conducteur s’est même arrêté le soir pour manger dans un endroit perdu avant de reprendre pour 3h de route x) Mais une fois arrivé à Pangandaran, on oublie totalement ce trajet et vous allez comprendre pourquoi :)
+    </p>
+    <h3>
+      Le logement à Pangandaran ?
+    </h3>
+    <p>
+      J'avoue que le choix de l'homestay est déterminant sur la qualité du séjour. Sans aucun regret nous avons choisi Mini Tiga Homestay! Le soir il y a toujours 2, 3 locaux avec une guitare qui mettent l'ambiance. Mais si on veut être tranquille, le premier étage a un super espace commun. Les chambres sont bien, rien à dire sur la propreté. Le petit-déjeuner est super ! Banana pancake!
+    </p>
+    <h3>
+      Que faire à Pangandaran ?
+    </h3>
+    <p>
+      1 jour green canyon / green valley : de la nature, de l'eau, du canyoning (léger)
+    </p>
+    <p>
+      1 jour découverte du surf le matin, barbecue sur la plage avec poisson frais et légumes cuisiné de façon local, l'après midi massage sur la plage, photo avec des locaux (peu habitué à voir des occidentaux) et baignade :)
+      Si vous vous entendez bien avec les locaux qui organisent les activités, ils vous proposeront d'aller boire un verre avec eux le soir au bamboo café.
+    </p>
+    <img src="@/assets/article/pangandaran/barbecue.jpg" alt="Barbecue on the beach after surfing">
+    <p>
+      On peut rallonger le séjour pour prendre plus de cours de surf, ou pour d'autres activités que je n'ai pas essayé. Le green canyon / valley et le surf étaient des activités proposées par l'homestay.
+    </p>
+    <h3>
+      Aller vers Jogja ?
+    </h3>
+    <p>
+      On peut faire le trajet entièrement en bus. Ou alors récupérer le train un peu plus au nord en prenant le taxi. Nous avons opté pour un trajet de nuit pour économiser une nuit en même temps (même si la voiture est plus ou moins agréable pour dormir). Le chauffeur nous a déposé à 3h du matin à borobudur pour faire le lever du soleil (450k IDR !!!) et ensuite nous a déposé à nos hôtel à Jogja. Le point positif est la nuit économisée mais le point négatif est le prix du lever du soleil ! Avec beaucoup trop de touristes. Je ne suis pas sûr que ça valait le coup.
+    </p>
+    <img src="@/assets/article/pangandaran/indonesian_flag.jpg" alt="Indonesian flag floatting in the air">
+  </article>
+</v-container>
 </template>
