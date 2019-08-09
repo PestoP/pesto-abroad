4de8fd8ac95fca70d6ee9a0222f8b2f3cb35ac96
PestoP <zanardo.florian@gmail.com>
2019-08-09 11:24:45 +0200

    feat(article list): return articles which match with the country
    Looks like a big 'if' is coming right away from hell

---
+++ src/constants/articlesListAsia.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    name: 'Pangandaran',
+    componentName: 'Pangandaran',
+    imageUrl: 'pangandaran/white-sand-beach',
+    linkTo: 'article',
+    description: `Entre Jakarta et Yogyakarta, Pangandaran permet un stop très agréable où surf, canyoning et détente sont au rendez-vous...`
+  },
+  {
+    name: 'Prambanan',
+    componentName: 'Prambanan',
+    imageUrl: 'prambanan/temple_vue_de_loin',
+    linkTo: 'article',
+    description: `Un temple qui vaut le détour, bien que très cher par rapport au coût de la vie indonnésienne. Il rappelle un peu Angkor Wat au Cambodge`
+  },
+  {
+    name: 'Borobudur',
+    componentName: 'Borobudur',
+    imageUrl: 'borobudur/borobudur_photo_couverture',
+    linkTo: 'article',
+    description: `Un temple pas comme les autres, il ne ressemble à aucun autre temple que j'ai pu visiter en Asie du sud-est ! Il vaut le détour, cependant peut-être pas pour le lever du soleil...`
+  },
+  {
+    name: 'Yogyakarta',
+    componentName: 'Yogyakarta',
+    imageUrl: 'yogyakarta/yogyakarta_lettre',
+    linkTo: 'article',
+    description: `Une ville qui n'a pas grand intérêt, si ce n'est son emplacement proche des temples Borobudur et Prambanan`
+  },
+  {
+    name: 'Mont Bromo',
+    componentName: 'Bromo',
+    imageUrl: 'bromo/bromo_de_loin',
+    linkTo: 'article',
+    description: `Une des plus belles merveilles que j'ai rencontré en Asie du sud-est, un immanquable si vous allez sur l'île de Java !`
+  },
+  {
+    name: 'Mont Kawah Ijen',
+    componentName: 'Ijen',
+    imageUrl: 'ijen/ijen_cratere',
+    linkTo: 'article',
+    description: `L'ascension n'est pas extraordinaire mais ça en vaut la chandelle, 'Blue fire' puis un lever de soleil sur le lac acide le plus grand du monde...`
+  },
+  {
+    name: 'Pemuteran',
+    componentName: 'Pemuteran',
+    imageUrl: 'pemuteran/poissons_pemuteran',
+    linkTo: 'article',
+    description: `Un village qui ressemble à une longue rue au bord de la route principale. Il réserve de la Tranquillité et du snorkeling`
+  },
+  {
+    name: 'Munduk',
+    componentName: 'Munduk',
+    imageUrl: 'munduk/jatiluwih_lettre',
+    linkTo: 'article',
+    description: `Étape à l'écart de la foule de Bali. Des jolies cascades bien qu'elles soient payantes, mais c'est surtout la porte d'accès à un circuit allant jusqu'au rizières de Jatiluwih...`
+  },
+  {
+    name: 'Ubud',
+    componentName: 'Ubud',
+    imageUrl: 'ubud/ubud_palace_nuit',
+    linkTo: 'article',
+    description: `La foule y est présente car c'est un endroit stratégique de Bali, beaucoup de beau temples son aux alentours, une très belle campagne. On peut aussi y faire un cours de cuisine Balinaise.`
+  },
+  {
+    name: 'Amed',
+    componentName: 'Amed',
+    imageUrl: 'amed/peinture_bateau',
+    linkTo: 'article',
+    description: `Plage, détente, yoga, ambiance reggea... Cet endroit est aussi reconnue pour ses 3 spots extraordinaires de snorkeling !`
+  },
+  {
+    name: 'Nusa Penida',
+    componentName: 'NusaPenida',
+    imageUrl: 'nusa_penida/kelingking_beach_serviette',
+    linkTo: 'article',
+    description: `Île encore un peu sauvage tout près de Bali, d'innombrables spots à voir. Un scooter et la liberté est dans vos mains, MA-GNI-FI-QUE !`
+  },
+  {
+    name: 'Le sud de Bali',
+    componentName: 'SouthBali',
+    imageUrl: 'sud_bali/mojito_ulluwatu',
+    linkTo: 'article',
+    description: `Ulluwatu, Jimbaran, Kuta, etc. Un repère de surfeur et de gens qui veulent faire des soirées ou des achats. De temps en temps, ça fait du bien.`
+  }
+]
