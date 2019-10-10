a490db5daec195924e8d90710d84387bb576edc4
PestoP <zanardo.florian@gmail.com>
2019-10-10 21:37:32 +0200

    feat(south bali): initialize article

--- src/views/articles/indonesia/SouthBali.vue
+++ src/views/articles/indonesia/SouthBali.vue
@@ -1,3 +1,25 @@
 <template>
-  <span>lol</span>
+  <v-container>
+
+  <article class='article'>
+    <header>
+      <h2>Le titre de l'article</h2>
+    </header>
+    <section class='article-body'>
+      <h3>
+        Premier titre de paragraphe ?
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam itaque accusantium esse molestiae et illum at cum voluptatem culpa? Laboriosam modi voluptatum inventore officiis dolores, ullam et veritatis distinctio laborum.
+      </p>
+      <ImageWithDescription src="pangandaran/indonesian_flag" alt="Indonesian flag floatting in the air" description='Indonesian flag floatting in the air at the beach' />
+    </section>
+  </article>
+</v-container>
 </template>
+
+<style lang="scss" scoped>
+  header {
+    background-image: url('~@/assets/article/pangandaran/article_background.jpg');
+  }
+</style>
